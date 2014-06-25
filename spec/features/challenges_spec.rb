00c95ad6784c328f9f7ef93a73752ca919025342
pdebelak <pdebelak@gmail.com>
2014-06-25 16:11:23 -0500

    Fails to create challenge with invalid challenger

--- spec/features/challenges_spec.rb
+++ spec/features/challenges_spec.rb
@@ -17,5 +17,24 @@ feature 'New Challenge' do
       fill_in 'end_date', with: '06/30/2014'
     end
     expect{ click_on 'FIGHT!' }.to change{ user.challenges.count }.by(1)
+    expect(current_url).to eq user_challenges_url(user)
+  end
+
+  scenario 'wont create a challenge if the challenger doesnt exist' do
+    visit '/'
+    within('form') do
+      fill_in 'email', with: 'pdebelak@example.com'
+      fill_in 'password', with: '123456'
+    end
+    click_on 'Login'
+    visit "/users/#{user.id}/challenges/new"
+    within('form') do
+      fill_in 'challenger_email', with: 'ysim@example.com'
+      fill_in 'start_date', with: '06/25/2014'
+      fill_in 'end_date', with: '06/30/2014'
+    end
+    expect{ click_on 'FIGHT!' }.to change{ user.challenges.count }.by(0)
+    expect(current_path).to eq "/users/#{user.id}/challenges"
+    expect(page).to have_text 'There is no challenger with that email.'
   end
 end
