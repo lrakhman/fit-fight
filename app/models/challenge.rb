fd3b6866f2ab7e4bda6f92eda343ce84b145a920
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-06-25 17:30:33 -0500

    initial challenge commit

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,4 +4,13 @@ class Challenge < ActiveRecord::Base
 	has_one :challenger, class_name: "User"
 	has_many :user_workouts, through: :user, source: :daily_workouts
 	has_many :challenger_workouts, through: :challenger, source: :daily_workouts
+
+	def user_points
+
+	end
+
+	def challenger_points
+		
+	end
+
 end
