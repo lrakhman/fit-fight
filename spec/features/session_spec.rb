7db60bcf46566f83ac827f47e95fa3b06f304a3d
pdebelak <pdebelak@gmail.com>
2014-06-25 14:40:30 -0500

    Able to login, rejects bad credentials

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -2,23 +2,21 @@ require 'spec_helper'
 
 feature 'Login In' do
 
-  before do
-    User.create(email: 'pdebelak@example.com', password: '123456', fullname: 'Peter Debelak')
-  end
+  let! (:user) { User.create(email: 'pdebelak@example.com', password: '123456', fullname: 'Peter Debelak') }
 
   context 'with valid credentials' do
     scenario 'signs me in' do
       # Given I'm on the login page
       visit '/'
       # And I have filled in the form with valid credentials
-      within('#authenticate') do
-        fill_in 'Email', with: 'pdebelak@example.com'
-        fill_in 'Password', with: '123456'
+      within('form') do
+        fill_in 'email', with: 'pdebelak@example.com'
+        fill_in 'password', with: '123456'
       end
       # When I click the login button
       click_on 'Login'
       # Then I should be on a page that says welcome back
-      expect(page).to have_text('Welcome Back')
+      expect(current_url).to eq(user_challenges_url(user))
     end
   end
 
@@ -27,13 +25,13 @@ feature 'Login In' do
        # Given I'm on the login page
       visit '/'
       # And I have filled in the form with valid credentials
-      within('#authenticate') do
-        fill_in 'Email', with: 'pdebelak@example.co.uk'
-        fill_in 'Password', with: '654321'
+      within('form') do
+        fill_in 'email', with: 'pdebelak@example.co.uk'
+        fill_in 'password', with: '654321'
       end
       click_on 'Login'
       # Then I should be on the login page
-      expect(page).to have_text("Invalid Credentials")
+      expect(page).to have_text("Your email or password is incorrect.")
     end
   end
 
