6f7aa7e186ca7ad9c7e0094812ad1f01c2f4a7b2
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-06-26 11:13:03 -0500

    add helper methods for point calculations

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -2,4 +2,15 @@ module ChallengesHelper
 	def has_challenge_for_current_date?(user)
 		user.daily_workouts.first.date == Time.now.strftime("%Y-%m-%d")
 	end
+
+	def point_assignment_for_steps
+	end
+
+	def point_assignment_for_distance
+	end
+
+	def point_assignment_for_active_time
+	end
+
+
 end
