13799a977029fa032807b0ced11c8702d797a5b9
lrakhman <lana.rakhman@gmail.com>
2014-06-26 16:47:55 -0500

    changed db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140626161028) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authorizations", force: true do |t|
     t.string   "provider"
     t.string   "uid"
@@ -55,6 +58,6 @@ ActiveRecord::Schema.define(version: 20140626161028) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
