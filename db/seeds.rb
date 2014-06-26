df4074f4d5aadd323c1ae823198fd4d261c64bb7
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-06-26 14:30:15 -0500

    integration with new challenges index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@
 User.create(fullname: "Mikee Pourhadi", email: "mikeepourhadi@gmail.com", password: "warcraft")
 User.create(fullname: "Peter Debelak", email: "petersmells@gmail.com", password: "warcraft")
 User.create(fullname: "Steve Yim", email: "steve@gmail.com", password: "warcraft")
+
+DailyWorkout.create(date: "2014-04-08", user_id: 1, steps: 2000, distance: 2, active_time: 40)
+DailyWorkout.create(date: "2014-04-09", user_id: 1, steps: 3000, distance: 3, active_time: 20)
+DailyWorkout.create(date: "2014-04-09", user_id: 2, steps: 2000, distance: 2, active_time: 40)
+DailyWorkout.create(date: "2014-04-08", user_id: 2, steps: 3000, distance: 1, active_time: 31)
