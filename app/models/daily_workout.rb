04d22b8c95425b73d40816bf870a0911d253adbf
lrakhman <lana.rakhman@gmail.com>
2014-06-26 21:36:21 -0500

    whenever gem works. FUCK YES.

--- app/models/daily_workout.rb
+++ app/models/daily_workout.rb
@@ -1,11 +1,5 @@
 class DailyWorkout < ActiveRecord::Base
 	belongs_to :user
 
-
-# 	client = Fitgem::Client.new {
-#   :consumer_key => my_key,
-#   :consumer_secret => my_secret,
-#   :token => user.oauth_token,
-#   :secret => user.oauth_secret
-# }
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ActiveRecord::Base
 	# validates :email, uniqueness: true, :format => /.+@.+\..+/
 	has_many :challenges
 	has_many :daily_workouts
+	has_one :most_recent_workout, -> { 'ORDER date, desc, limit 1' }
 
 	has_many :authorizations
 
+	# def sync
+
+	# 	make client, call client.activity_on_date for each date that there is no daily workout, create a daily workout for each of those days up to today, find or create by today
+	# end
+
+	def self.sing
+		puts "Helloooooooo, there!"
+	end
 
 	def total_steps
 		steps_array = self.daily_workouts.map {|workout| workout.steps}
